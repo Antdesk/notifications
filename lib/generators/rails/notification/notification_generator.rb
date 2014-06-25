99766a405aaea5cdd4deb176f3cd448f93baf6af
Magic <magic@antdesk>
2014-06-25 17:59:50 +0200

    g v2

--- lib/generators/rails/notification/notification_generator.rb
+++ lib/generators/rails/notification/notification_generator.rb
@@ -1,6 +1,11 @@
 module Rails
   module Generators
-    class NotificationGenerator < NamedBase #metagenerator
+    class NotificationGenerator < Base #metagenerator
+      argument :model_name, :type => :string
+      class_option :create, :type => :boolean, :default => false, :description => "Add create action"
+      class_option :update, :type => :boolean, :default => false, :description => "Add update action"
+      class_option :destroy, :type => :boolean, :default => false, :description => "Add destroy action"
+      class_option :random, :type => :boolean, :default => false, :description => "Add random action"
       hook_for :orm, :required => true
       source_root File.expand_path("../templates", __FILE__)
 
@@ -9,7 +14,7 @@ module Rails
       end
 
       def create_observer_file
-        template 'notification.rb', File.join('app/models', class_path, "virtual_machine_observer.rb")
+        template 'notification.rb', File.join('app/models', class_path, "virtual_machine_observer.rb") if options.create?
       end
     end
   end
