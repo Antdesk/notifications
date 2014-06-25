cc5b6a77953effd67d61f0030512ecc134e2c60c
Magic <magic@antdesk>
2014-06-25 16:14:58 +0200

    g v2

--- lib/generators/rails/notification/notification_generator.rb
+++ lib/generators/rails/notification/notification_generator.rb
@@ -2,10 +2,15 @@ module Rails
   module Generators
     class NotificationGenerator < Base #metagenerator
       hook_for :orm, :required => true
+      check_class_collision :suffix => "Observer"
       source_root File.expand_path("../templates", __FILE__)
 
       def add_my_initializer
         template 'initializer.rb', 'config/initializers/notifications_initializer.rb'
+      end
+
+      def create_observer_file
+        template 'notification.rb', File.join('app/models', class_path, "virtual_machine_observer.rb")
       end
     end
   end
