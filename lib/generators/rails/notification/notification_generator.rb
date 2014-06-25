2c47460367c4ebe18ea4ad046b4929d78a99241c
Magic <magic@antdesk>
2014-06-25 15:59:56 +0200

    g v2

--- lib/generators/rails/notification/notification_generator.rb
+++ lib/generators/rails/notification/notification_generator.rb
@@ -2,6 +2,11 @@ module Rails
   module Generators
     class NotificationGenerator < NamedBase #metagenerator
       hook_for :orm, :required => true
+      source_root File.expand_path("../templates", __FILE__)
+
+      def add_my_initializer
+        template 'initializer.rb', 'config/initializers/notifications_initializer.rb'
+      end
     end
   end
 end
