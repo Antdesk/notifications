4c213bcc2517b9925b1ba27c621fa34a034ce06c
Magic <magic@antdesk>
2014-06-26 11:49:56 +0200

    test

--- lib/generators/rails/notification/templates/notification.rb
+++ lib/generators/rails/notification/templates/notification.rb
@@ -1,6 +1,6 @@
-class VirtualMachineObserver < ActiveRecord::Observer
-  def after_create(virtual_machine)
+class <%= argument %>Observer < ActiveRecord::Observer
+  def after_create(para)
     Rails.logger.warn('create')
-    Rails.logger.warn(bird)
+    Rails.logger.warn(para)
   end
 end
